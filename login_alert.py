6a955492ae86eae55946e947571b1c8b89af219f
MrViper111 <74232706+MrViper111@users.noreply.github.com>
2022-06-28 18:13:39 -0700

    Create login_alert.py

---
+++ login_alert.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import requests
+
+server_ip = requests.get("https://api.ipify.org").text
+panel_page = "" # If the server is hosted externally, add a link to the panel here. Otherwise, leave it blank
+webhook_url = "" # Put the Discord webhook URL here
+
+embed = {
+    "description": f"Someone has logged into `{server_ip}`! If this was not you, please power off the machine and change the password [here]({linode_page})",
+    "title": ":inbox_tray: Logged In",
+    "color": 2752256
+}
+
+data = {
+    "content": "",
+    "username": server_ip,
+    "embeds": [
+        embed
+    ]
+}
+
+
+result = requests.post(
+    webhook_url,
+    json=data
+)
+
+try:
+    result.raise_for_status()
+except requests.exeptions.HTTPError as error:
+    print(f"[ERROR] {error}")
+else:
+    pass
